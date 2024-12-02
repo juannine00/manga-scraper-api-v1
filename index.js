084a858660511424c02c6f66c9eab09e5e4da7c6
juannine00 <137662269+juannine00@users.noreply.github.com>
2024-12-02 20:28:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const expressListEndpoints = require('express-list-endpoints');
 
+
 const app = express();
 const PORT = 3000;
 
@@ -73,6 +74,8 @@ const scrapeComicDetail = async (comicPath) => {
             chapters.push({ title: chapterTitle, link: chapterLink, time: chapterTime });
         });
 
+        
+
         return {
             imageUrl,
             title,
@@ -94,25 +97,25 @@ const scrapeComicDetail = async (comicPath) => {
     }
 };
 
-// **Perbaikan:** Fungsi untuk mengambil detail chapter
-const chapterDetail = async (chapterPath) => {
+const champterDetail = async (champterPath) => {
     try {
-        const { data } = await axios.get(`${BASE_URL}/${chapterPath}`);
+        const { data } = await axios.get(`${BASE_URL}/${champterPath}`);
         const $ = cheerio.load(data);
 
         const title = $('div.chapter_headpost h1').text().trim();
-        const nextChapter = $('div.nextprev a[rel="next"]').attr('href');
-        const images = []; 
+        const nextchapter = $('div.nextprev a[rel="next"]').attr('href');
+        const image = []; 
         $('div.chapter_body div.main-reading-area').each((i, el) => {
             $(el).find('img').each((j, img) => {
-                images.push($(img).attr('src'));
+                image.push($(img).attr('src'));
             });
         });
 
+
         return {
             title,
-            nextChapter,
-            images
+            nextchapter,
+            image
         };
     } catch (error) {
         console.error(error);
@@ -120,101 +123,291 @@ const chapterDetail = async (chapterPath) => {
     }
 };
 
-// Route untuk mengambil daftar populer
+const search = async (query, page) => {
+    const { data } = await axios.get(`${BASE_URL}/page/${page}/?s=${query}`);
+    const $ = cheerio.load(data);
+    const results = [];
+
+    $('div.list-update_item').each((i, element) => {
+        const title = $(element).find('h3.title').text().trim();
+        const image = $(element).find('div.list-update_item-image img.ts-post-image').attr('src');
+        const chapter = $(element).find('div.chapter').text().trim();
+        const rating = $(element).find('div.rating-bintang span').css('width');
+        const type = $(element).find('span.type').text().trim();
+        const link = $(element).find('a.data-tooltip').attr('href');
+        const path = new URL(link).pathname;
+
+        results.push({
+            title,
+            image,
+            chapter, 
+            rating: parseFloat(rating) / 10,
+            type,
+            link: path,
+        });
+    });
+
+    return results;
+};
+
+const scrapeGenre = async (genre , page) => {
+    try {
+        const { data } = await axios.get(`${BASE_URL}/genres/${genre}/page/${page}`);
+        const $ = cheerio.load(data);
+        const results = [];
+
+        $('div.list-update_item').each((i, element) => {
+            const title = $(element).find('h3.title').text().trim();
+            const image = $(element).find('div.list-update_item-image img.ts-post-image').attr('src');
+            const chapter = $(element).find('div.chapter').text().trim();
+            const rating = $(element).find('div.rating-bintang span').css('width');
+            const type = $(element).find('span.type').text().trim();
+            const link = $(element).find('a.data-tooltip').attr('href');
+            const path = new URL(link).pathname;
+
+            results.push({
+                title,
+                image,
+                chapter,
+                rating: parseFloat(rating) / 10,
+                type,
+                link: path,
+            });
+        });
+
+        return results;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+const genreList = async () => {
+    try {
+        const { data } = await axios.get(`${BASE_URL}`);
+        const $ = cheerio.load(data);
+        const results = [];
+
+        $('div.section ul.genre li a').each((i, element) => {
+            const genre = $(element).text().trim();
+            const link = $(element).attr('href').replace(BASE_URL, '');
+            results.push({
+                genre,
+                link
+            });
+        });
+
+        return results;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+const scrapeType = async (type, page) => {
+    try {
+        const { data } = await axios.get(`${BASE_URL}/daftar-komik/page/${page}/?status&type=${type}&orderby=titleasc`);
+        const $ = cheerio.load(data);
+        const results = [];
+
+        $('div.list-update_item').each((i, element) => {
+            const title = $(element).find('h3.title').text().trim();
+            const image = $(element).find('div.list-update_item-image img.ts-post-image').attr('src');
+            const chapter = $(element).find('div.chapter').text().trim();
+            const rating = $(element).find('div.rating-bintang span').css('width');
+            const type = $(element).find('span.type').text().trim();
+            const link = $(element).find('a.data-tooltip').attr('href');
+            const path = new URL(link).pathname;
+
+            results.push({
+                title,
+                image,
+                chapter,
+                rating: parseFloat(rating) / 10,
+                type,
+                link: path,
+            });
+        });
+
+        return results;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+const scrapeOngoing = async (page) => {
+    try {
+        const { data } = await axios.get(`${BASE_URL}/daftar-komik/page/${page}/?status=Ongoing&type&orderby=titleasc`);
+        const $ = cheerio.load(data);
+        const results = [];
+
+        $('div.list-update_item').each((i, element) => {
+            const title = $(element).find('h3.title').text().trim();
+            const image = $(element).find('div.list-update_item-image img.ts-post-image').attr('src');
+            const chapter = $(element).find('div.chapter').text().trim();
+            const rating = $(element).find('div.rating-bintang span').css('width');
+            const type = $(element).find('span.type').text().trim();
+            const link = $(element).find('a.data-tooltip').attr('href');
+            const path = new URL(link).pathname;
+
+            results.push({
+                title,
+                image,
+                chapter,
+                rating: parseFloat(rating) / 10,
+                type,
+                link: path,
+            });
+        });
+
+        return results;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+const scrapeCompleted = async (page) => {
+    try {
+        const { data } = await axios.get(`${BASE_URL}/daftar-komik/page/${page}/?status=Completed &type&orderby=titleasc`);
+        const $ = cheerio.load(data);
+        const results = [];
+
+        $('div.list-update_item').each((i, element) => {
+            const title = $(element).find('h3.title').text().trim();
+            const image = $(element).find('div.list-update_item-image img.ts-post-image').attr('src');
+            const chapter = $(element).find('div.chapter').text().trim();
+            const rating = $(element).find('div.rating-bintang span').css('width');
+            const type = $(element).find('span.type').text().trim();
+            const link = $(element).find('a.data-tooltip').attr('href');
+            const path = new URL(link).pathname;
+
+            results.push({
+                title,
+                image,
+                chapter,
+                rating: parseFloat(rating) / 10,
+                type,
+                link: path,
+            });
+        });
+
+        return results;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+
+app.get('/', (req, res) => {
+    const endpoints = expressListEndpoints(app);
+    const formattedEndpoints = endpoints.map(endpoint => {
+      return {
+        path: endpoint.path,
+        methods: endpoint.methods,
+        middlewares: endpoint.middleware,
+        details: {
+          description: `Handles ${endpoint.methods.join(', ')} requests for ${endpoint.path}`
+        }
+      };
+    });
+    
+    res.json({
+      message: "List of available endpoints",
+      count: formattedEndpoints.length,
+      endpoints: formattedEndpoints
+    });
+  });
+// Route to get the list of popular
 app.get('/popular/:page', async (req, res) => {
     const { page } = req.params;
     const comics = await scrapePage(page);
     res.json(comics);
 });
 
-// Route untuk mengambil detail komik berdasarkan judul
+// Route to get the details of a specific comic
 app.get('/komik/:title', async (req, res) => {
     const { title } = req.params;
     const comicDetail = await scrapeComicDetail(title);
     if (!comicDetail) {
-        res.status(404).json({ message: 'Comic not found' });
+      res.status(404).json({ message: 'Comic not found' });
     } else {
-        res.json(comicDetail);
+      res.json(comicDetail);
     }
-});
+  });
 
-// **Perbaikan:** Route untuk mengambil detail chapter
-app.get('/chapter/:title', async (req, res) => {
+app.get ('/chapter/:title', async (req, res) => {
     const { title } = req.params;
-    const chapterDetailResult = await chapterDetail(title);  // Memanggil fungsi yang benar
-    if (!chapterDetailResult) {
+    const chapterDetail = await champterDetail(title);
+    if (!chapterDetail) {
         res.status(404).json({ message: 'Chapter not found' });
     } else {
-        res.json(chapterDetailResult);
+        res.json(chapterDetail);
     }
 });
 
-// Route untuk pencarian komik
 app.get('/search/:query/:page?', async (req, res) => {
     try {
-        const { query, page = 1 } = req.params;
-        const results = await search(query, page);
-        if (!results) {
-            res.status(404).json({ message: 'No results found' });
-        } else {
-            res.json(results);
-        }
+      const { query, page = 1 } = req.params;
+      const results = await search(query, page);
+      if (!results) {
+        res.status(404).json({ message: 'No results found' });
+      } else {
+        res.json(results);
+      }
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
     }
-});
+  });
 
-// Route untuk mengambil daftar genre
 app.get('/genres/:genre/:page?', async (req, res) => {
     const { genre, page = 1 } = req.params;
     const results = await scrapeGenre(genre, page);
     if (!results) {
-        res.status(404).json({ message: 'No results found' });
+      res.status(404).json({ message: 'No results found' });
     } else {
-        res.json(results);
+      res.json(results);
     }
-});
+  });
 
 app.get('/genres', async (req, res) => {
     const results = await genreList();
     res.json(results);
 });
 
-// Route untuk mengambil berdasarkan tipe
 app.get('/types/:type/:page?', async (req, res) => {
-    const { type, page = 1 } = req.params;
-    const allowedTypes = ['manga', 'manhua', 'manhwa'];
-    if (!allowedTypes.includes(type)) {
-        return res.status(400).json({ error: 'Invalid type' });
-    }
-    const results = await scrapeType(type, page);
-    res.json(results);
+  const { type, page = 1 } = req.params;
+  const allowedTypes = ['manga', 'manhua', 'manhwa'];
+  if (!allowedTypes.includes(type)) {
+    return res.status(400).json({ error: 'Invalid type' });
+  }
+  const results = await scrapeType(type, page);
+  res.json(results);
 });
 
-// Route untuk mengambil komik yang sedang berlangsung
 app.get('/ongoing/:page?', async (req, res) => {
-    const { page = 1 } = req.params;
-    const results = await scrapeOngoing(page);
-    if (!results) {
-        res.status(404).json({ message: 'No results found' });
-    } else {
-        res.json(results);
-    }
+  const { page = 1 } = req.params;
+  const results = await scrapeOngoing(page);
+  if (!results) {
+    res.status(404).json({ message: 'No results found' });
+  } else {
+    res.json(results);
+  }
 });
 
-// Route untuk mengambil komik yang sudah selesai
+
 app.get('/completed/:page?', async (req, res) => {
     const { page = 1 } = req.params;
     const results = await scrapeCompleted(page);
     if (!results) {
         res.status(404).json({ message: 'No results found' });
-    } else {
+      } else {
         res.json(results);
-    }
-});
-
-// Menjalankan server
+      }
+  });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
